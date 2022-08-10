04119330e0b204517baff88eda4ea91730c4a52e
britneyfrizzell@gmail.com <95828527+bfrizzell93@users.noreply.github.com>
2022-08-10 17:19:57 -0500

    Add task button works and toggles to close

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState (false)
   const [tasks, setTasks] = useState([
     {   id: 1,
         text: "Grocery Shopping",
@@ -21,6 +23,13 @@ const App = () => {
     },
     ])
 
+    //Add task
+    const addTask = (task) => {
+      const id= Math.floor(Math.random()* 10000 +1)
+      const newTask = {id,...task} //assigns a random id to a task
+      setTasks([...tasks, newTask]) //adds new task to list
+    }
+
     //Delete task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task)=>task.id !== id))
@@ -33,7 +42,8 @@ const App = () => {
   
   return (
     <div className='container'> 
-      <Header title="Task Tracker" />
+      <Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No Tasks to show."}
     </div>
   );
